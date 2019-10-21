5bc07b66646aaff087c4ce9a20faee9b2bc4de19
changkyun Ahn <oops0429@gmail.com>
2019-10-21 23:38:22 +0900

    simulation2

---
+++ algorithm_013/algorithm_013/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm_013
+//
+//  Created by mac_ck on 21/10/2019.
+//  Copyright © 2019 cpp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
