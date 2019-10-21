972122b91413682ca24931c02f09b682ee0fbde2
changkyun Ahn <oops0429@gmail.com>
2019-10-21 23:45:29 +0900

    test

---
+++ algorithm_013/algorithm_013/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  algorithm_013
+//
+//  Created by mac_ck on 21/10/2019.
+//  Copyright © 2019 cpp. All rights reserved.
+//
+
+#include "test.hpp"
---
+++ algorithm_013/algorithm_013/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  algorithm_013
+//
+//  Created by mac_ck on 21/10/2019.
+//  Copyright © 2019 cpp. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
