b8dddf1f2396e42967ef5e084f5c6e14676bd372
Ethan <ckethan0429@gmail.com>
2020-01-07 14:22:18 +0900

    insert sort review

--- algorithm_036/algorithm_036/main.cpp
+++ algorithm_036/algorithm_036/main.cpp
@@ -7,28 +7,33 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 int main(){
-    int a[101];
-    int i,j,n,tmp;
-    cin >> n;
-    for (i=0; i<n; i++){
-        cin >> a[i];
+    int a[100], n, i, j, tmp;
+    scanf("%d", &n);
+    
+    for(i=0; i<n; i++){
+        scanf("%d", &a[i]);
     }
+    
+    // 정렬 시작
     
     for(i=1; i<n; i++){
         tmp = a[i];
         for(j=i-1; j>=0; j--){
-            if (a[j] > tmp){
-                a[j+1] = a[j];
-            }
+            if(a[j] > tmp) a[j+1] = a[j];
             else break;
         }
         a[j+1] = tmp;
     }
     
-    for (i=0; i<n; i++){
-        cout << a[i] << " ";
+    // 정렬 끝
+    
+    for (i=0; i<n; i++) {
+        printf("%d ", a[i]);
     }
+    
 }
